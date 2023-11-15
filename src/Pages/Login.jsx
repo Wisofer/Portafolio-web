076e1f014af9e73ac19f7baac4d7f677e7d91293
@wisofer 17 <87929107+Wisofer@users.noreply.github.com>
2023-11-15 14:12:50 -0600

    formulario 2.0

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -2,15 +2,19 @@ import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { supabase } from "../Components/Client";
-import '../App.css'
+import "../App.css";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(""); // Estado para el mensaje de error
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // Aquí puedes agregar la lógica para enviar los datos del formulario al servidor
+
+    setIsLoading(true);
+    setErrorMessage(""); // Reiniciar el mensaje de error al iniciar el inicio de sesión
 
     try {
       const resultado = await supabase.auth.signInWithPassword({
@@ -18,9 +22,13 @@ const Login = () => {
         password,
       });
 
-      
+      // Realizar acciones adicionales después de iniciar sesión exitosamente
+
     } catch (error) {
       console.log("El error fue:", error);
+      setErrorMessage("Contraseña incorrecta"); // Establecer el mensaje de error en caso de contraseña incorrecta
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -61,6 +69,9 @@ const Login = () => {
                 placeholder="Ingrese su contraseña"
                 onChange={(e) => setPassword(e.target.value)}
               />
+              {errorMessage && (
+                <p className="text-red-500 text-sm mt-1">{errorMessage}</p>
+              )}
             </div>
             <div className="flex items-center justify-between mb-4">
               <label htmlFor="remember" className="flex items-center">
@@ -77,8 +88,9 @@ const Login = () => {
             <button
               type="submit"
               className="bg-blue-500 text-white rounded-md px-4 py-2 w-full font-bold hover:bg-blue-600"
+              disabled={isLoading}
             >
-              Iniciar sesión
+              {isLoading ? "Cargando..." : "Iniciar sesión"}
             </button>
           </form>
         </div>
